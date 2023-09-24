08a8521f7ec71b29a5b3040b79a5c7772e0970c4
Daniel Constantin <hello@danielconstantin.net>
2023-09-24 13:54:29 +0300

    Use local file

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const nextConfig = {
     return [
       {
         source: '/api/pricing_info',
-        destination: 'https://deepwhale-ai-cloudformation-template-public.s3.us-east-1.amazonaws.com/deepwhale-ai-front-end-exercise/deepwhaleai_sample_data_fe_1st-step.csv',
+        destination: '/deepwhaleai_sample_data_fe_1st-step.csv',
       },
     ];
   },
